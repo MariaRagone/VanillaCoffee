e247622fc5557512cf74db4bb22ceece4c4a30b0
mragone <maria.ragone@samaritanministries.org>
2025-03-18 11:25:10 -0400

    added shadow DOM

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,6 +1,12 @@
 export class MenuPage extends HTMLElement {
   constructor() {
     super();
+    this.root = this.attachShadow({ mode: "open" });
+  }
+  connectedCallback() {
+    const template = document.getElementById("menu-page-template");
+    const content = template.content.cloneNode(true);
+    this.appendChild(content);
   }
 }
 customElements.define("menu-page", MenuPage);
